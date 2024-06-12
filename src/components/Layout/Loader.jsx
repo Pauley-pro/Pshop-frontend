eed610b800804637a0e0f29a47032ab048c66721
Paul Adelaja <pauladelaja54@gmail.com>
2024-06-12 16:22:30 +0100

    Still working on the failed deployment

--- src/components/Layout/Loader.jsx
+++ src/components/Layout/Loader.jsx
@@ -1,21 +1,15 @@
-import React from "react";
-import Lottie from "react-lottie";
-import animationData from "../../Assests/animations/24151-ecommerce-animation.json";
+import React from 'react';
+import "./Loader.css"
+
 
 const Loader = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <Lottie options={defaultOptions} width={300} height={300} />
-    </div>
-  );
+    return (
+        <div className="w-full h-screen flex items-center justify-center">
+            <div className="loader">
+
+            </div>
+        </div>
+    );
 };
 
 export default Loader;
